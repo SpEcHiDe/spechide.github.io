cedb4623796f480d717558a7a404b20779aba197
Shrimadhav U K <spechide@shrimadhavuk.me>
2016-07-28 09:42:58 +0530

    github-widget

--- js/github-widget.js
+++ js/github-widget.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-  $("body").append("<style>@import url(https://fonts.googleapis.com/css?family=Noto+Sans:400,700);a,a:hover{text-decoration:none}.container{display:flex;flex-direction:row;flex-wrap:no-wrap;align-items:center;justify-content:center;color:#333;font-family:'Noto Sans',sans-serif}.personal-details .bio,.stats .count{color:#4078C0}.github-widget{border:1px solid #DDD;max-width:350px}.item{flex:1;text-align:center;padding:10px}.repositories .language,.repositories .stars{text-align:left}.repositories .language div,.repositories .stars div{padding:5px 0}.photo{flex:2}.photo img{border-radius:100%;max-width:90px}.personal-details{flex:6}.personal-details .full-name{font-size:1.5em;line-height:1.5em}.personal-details .location{font-size:.8em}.stats .count{font-size:1.2em;font-weight:700}.repositories .names{flex:2;text-align:left}.repositories .names div{padding:5px 0;text-overflow:ellipsis}.follow{flex:2}.active-time{flex:4;font-size:.8em}.heading{font-weight:400;color:#666}hr{border:1px solid #DDD}a{color:#4078C0}.follow button{width:100%;height:2em;border:none;background:#ddd}</style>");
-
-  $(".github-widget").append('<div class="container"><div class="item photo"></div><div class="item personal-details"></div></div><div class="container stats"></div><hr><div class="container"><div class="item heading">Top repositories</div></div><div class="repositories"></div><div class="container"><div class="item follow"></div><div class="item active-time"></div></div>');
-
   var username = $(".github-widget").data("username");
   fetchRepos(username);
   fetchUserDetails(username);
@@ -47,7 +43,7 @@ function updateUserDetails(user) {
     $(".personal-details").append('<div class="bio">' + user.bio + '</div>')
   }
   if (user.location) {
-    $(".personal-details").append('<div class="location">&#9906; ' + user.location + '</div>');
+    $(".personal-details").append('<div class="location"><i class="fa fa-map-marker"></i> ' + user.location + '</div>');
   }
 
   $(".stats").append(' <div class="item"><div class="count">' + user.followers + '</div><div class="stat-name">Followers</div></div>');
@@ -66,7 +62,7 @@ function updateRepoDetails(repos) {
 
   for (var i = 0; i < repos.length; i++) {
     var language = repos[i].language ? repos[i].language : "Unknown";
-    $(".repositories").append('<div class="container"><div class="item names"><div><a href="' + repos[i].repoUrl + '">' + repos[i].name + '</a></div></div><div class="item language"><div>' + language + '</div></div><div class="item stars"><div>&#9733;' + repos[i].stars + '</div></div></div>');
+    $(".repositories").append('<div class="continer"><div class="item names"><div><a href="' + repos[i].repoUrl + '">' + repos[i].name + '</a></div></div><div class="item language"><div>' + language + '</div></div><div class="item stars"><div>&#9733;' + repos[i].stars + '</div></div></div>');
   }
 
 }
